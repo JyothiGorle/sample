359f3598d38538e0ddffc52631ff2ecc4f7b6917
Jyothigorle <jyothigorle2003@gmail.com>
2023-10-30 22:20:13 +0530

    commit2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hello world.
+          Edit <code>src/App.js</code> This is Jyothi.
         </p>
         <a
           className="App-link"
